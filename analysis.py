97412253e5a092a8efbfe699f7c3eccdf2b9d579
Nicolas Farrugia <nicolas.farrugia@imt-atlantique.fr>
2020-04-03 20:29:56 +0200

    new functions for analysis

--- analysis.py
+++ analysis.py
@@ -5,6 +5,8 @@ import datetime
 from matplotlib import pyplot as plt 
 
 from sklearn.manifold import TSNE
+from umap import UMAP
+
 
 def extract_subject(Df,partID):
     return Df[Df['id']==partID]
@@ -35,6 +37,14 @@ def process_tsne(Df):
 
 
 
+def process_umap(Df):
+    secs,allprobas,allembed,allndsi,night_day,labelmax = return_preprocessed(Df)
+    myproj = UMAP(n_components=2,metric='cosine')
+    embed_low = myproj.fit_transform(allembed)
+    return embed_low,secs,allprobas,allembed,allndsi,night_day,labelmax
+
+
+
 def keep_array_thresh(probasarray,threshold=0.1):
     array_max = np.max(probasarray,axis=0)
     ind_max = np.argwhere(array_max>threshold)
@@ -51,3 +61,67 @@ def inverse_search(allprobas,labels_all,label,Df):
     ind_max = np.argmax(probas)
     files = list(Df.file)
     return files[ind_max]
+
+
+
+def figure_embedding(Df):
+
+    embed_low,seconds_after,allprobas,allembed,allndsi,night_day,labelmax = process_umap(Df)
+    
+    fig = plt.figure(figsize=(10,10))
+
+    ax = plt.subplot(221)
+
+    scatter = ax.scatter(embed_low[:,0],embed_low[:,1],c=labelmax,alpha=0.7,cmap=plt.cm.tab20)
+    ind,indlabels = scatter.legend_elements(num=None)
+
+    whichlabels = np.argwhere(np.bincount(labelmax)).ravel()
+
+    labels_final = [labels[i] for i in whichlabels]
+
+    # produce a legend with the unique colors from the scatter
+    legend1 = ax.legend(ind,labels_final,
+                        loc="lower left", title="Classes",ncol=3,bbox_to_anchor=(0,1.1))
+    plt.title('Colored by Label')
+
+    ax = plt.subplot(222)
+
+    scatter = ax.scatter(embed_low[:,0],embed_low[:,1],c=night_day,alpha=0.8)
+    ind,indlabels = scatter.legend_elements(num=None)
+
+
+    # produce a legend with the unique colors from the scatter
+    legend1 = ax.legend(ind,['night','day'],
+                        loc="lower left", title="Classes",ncol=2)
+    plt.title('Colored by Night / Day')
+
+    #ax = plt.subplot(223)
+
+    #scatter = ax.scatter(embed_umap[:,0],embed_umap[:,1],c=allndsi,alpha=0.6,cmap=plt.cm.RdBu_r)
+    #fig.colorbar(scatter, ax=ax)
+    #plt.title('Colored by NDSI')
+
+
+
+
+
+    ax = plt.subplot(223)
+
+    allndsi_neg = (allndsi < 0 )
+
+    scatter = ax.scatter(embed_low[allndsi_neg,0],embed_low[allndsi_neg,1],c=allndsi[allndsi_neg],alpha=0.6,cmap=plt.cm.Blues_r)
+    fig.colorbar(scatter, ax=ax)
+    plt.title('Only negative NDSI')
+
+
+
+    ax = plt.subplot(224)
+
+    allndsi_pos = (allndsi > 0 )
+
+    scatter = ax.scatter(embed_low[allndsi_pos,0],embed_low[allndsi_pos,1],c=allndsi[allndsi_pos],alpha=0.6,cmap=plt.cm.spring)
+    fig.colorbar(scatter, ax=ax)
+    plt.title('Only positive NDSI')
+    
+    
+    return fig
